8021a671c3d82355cdaa6868987deae660da223c
EkaterinaZi <kat.zinovik@gmail.com>
2022-07-10 02:37:08 +0300

    home work

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryEl = galleryItems
+    .map(({ description, original, preview }) => `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`)
+    .join('');
+    
+gallery.insertAdjacentHTML("beforeend", galleryEl);
+
+let galleryLibrary = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
